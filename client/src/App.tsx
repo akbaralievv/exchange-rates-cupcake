8ac78bf2c84fd6e51a4da978da773b0582d92078
AkbaralievAlisher <kgkgg28@gmail.com>
2024-07-11 00:59:23 +0600

    feat: added logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,17 @@ import React from 'react';
 import ExchangeRatesTable from './components/ExchangeRatesTable';
 import useRates from './hooks/useRates';
 
+import cupcakeLogo from './assets/cupcake_logo.png';
+
 const App: React.FC = () => {
   const rates = useRates();
 
   return (
     <div className="App">
-      <h1>Exchange Rates</h1>
+      <div className="title">
+        <img src={cupcakeLogo} alt="logo" />
+        <h1>Exchange Rates</h1>
+      </div>
       <ExchangeRatesTable rates={rates} />
     </div>
   );
